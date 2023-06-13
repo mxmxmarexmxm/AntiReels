e7f5592c7110c4f8f1a7f115c7308b7468de673d
marinko malencic <marinko.malencic542@gmail.com>
2023-06-13 18:20:56 +0200

    chore: removed unncessary conditions to simplify code

--- youtube.js
+++ youtube.js
@@ -12,19 +12,17 @@ const sidebarMenuObserver = new MutationObserver(() => {
 sidebarMenuObserver.observe(document, { childList: true, subtree: true });
 
 const hideDirectShorts = () => {
-  let location = window.location.href;
-  if (location.includes('/shorts/')) {
-    let body = document.body;
-    body.innerHTML = '';
+  let body = document.body;
+  body.innerHTML = '';
 
-    let container = document.createElement('div');
-    container.style.height = '100vh';
-    container.style.paddingLeft = '20px';
-    container.style.paddingRight = '20px';
-    container.style.backgroundColor = 'black';
-    container.style.zIndex = 100000;
+  let container = document.createElement('div');
+  container.style.height = '100vh';
+  container.style.paddingLeft = '20px';
+  container.style.paddingRight = '20px';
+  container.style.backgroundColor = 'black';
+  container.style.zIndex = 100000;
 
-    container.innerHTML = `
+  container.innerHTML = `
     <div id="reel-blocker" style="display: flex; width: 100%; height: 100%; align-items: center; justify-content: center; text-align:center; color:white">
       <div style="display: flex; flex-direction:column; align-items: center; justify-content: center; background: linear-gradient(to bottom, #333, #111); border-radius: 20px; padding:10px; max-width:700px; min-width: 400px; aspect-ratio:1/1">
         <h1 style="font-size:4rem; margin-bottom: 1rem;">Stay Productive!</h1>
@@ -39,8 +37,7 @@ const hideDirectShorts = () => {
       </div> 
     </div>
   `;
-    body.appendChild(container);
-  }
+  body.appendChild(container);
 };
 
 // Observing location to block direct link shorts
