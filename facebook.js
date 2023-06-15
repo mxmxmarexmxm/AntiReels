4adab9246bd270cc640d6db92b0d11d84b4bacda
marinko malencic <marinko.malencic542@gmail.com>
2023-06-15 18:48:14 +0200

    fix: Fix unpredicted behavior in blockDirectReel function

--- facebook.js
+++ facebook.js
@@ -12,16 +12,18 @@ const sidebarMenuObserver = new MutationObserver(() => {
 sidebarMenuObserver.observe(document, { childList: true, subtree: true });
 
 const hideDirectReel = () => {
-  const reelDiv = document.querySelector('div[role="main"]');
-  reelDiv.innerHTML = '';
+  let location = window.location.href;
+  if (location.includes('/reel/')) {
+    const reelDiv = document.querySelector('div[role="main"]');
+    reelDiv.innerHTML = '';
 
-  let container = document.createElement('div');
-  container.style.height = '100vh';
-  container.style.paddingLeft = '20px';
-  container.style.paddingRight = '20px';
-  container.style.backgroundColor = 'black';
+    let container = document.createElement('div');
+    container.style.height = '100vh';
+    container.style.paddingLeft = '20px';
+    container.style.paddingRight = '20px';
+    container.style.backgroundColor = 'black';
 
-  container.innerHTML = `
+    container.innerHTML = `
     <div id="reel-blocker" style="display: flex; width: 100%; height: 100%; align-items: center; justify-content: center; text-align:center; color:white">
       <div style="display: flex; flex-direction:column; align-items: center; justify-content: center; background: linear-gradient(to bottom, #333, #111); border-radius: 20px; padding:10px; max-width:500px; min-width: 300px; aspect-ratio:1/1">
         <h1 style="font-size:2rem; margin-bottom: 1rem; color: white;">Stay Productive!</h1>
@@ -33,16 +35,17 @@ const hideDirectReel = () => {
       </div> 
     </div>
   `;
-  reelDiv.appendChild(container);
+    reelDiv.appendChild(container);
+  }
 };
 
 // Observing location to block direct link shorts
 let prevLocation;
 const locationObserver = new MutationObserver(() => {
   let location = window.location.href;
-  if (location.includes('/reel/') && location !== prevLocation) {
-    hideDirectReel();
+  if (location !== prevLocation) {
     prevLocation = location;
+    hideDirectReel();
   }
 });
 
