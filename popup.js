9589688af2d5bcaf80ac00e052ea23ded37d7095
marinko malencic <marinko.malencic542@gmail.com>
2023-08-15 23:11:24 +0200

    chore: update popup.js

--- popup.js
+++ popup.js
@@ -1,4 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
   const checkbox = document.getElementById('ig');
-  console.log(checkbox.checked);
+
+  checkbox.addEventListener('change', function () {
+    browser.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      const tabId = tabs[0].id;
+
+      if (checkbox.checked) {
+        browser.tabs.sendMessage(tabId, { action: 'enableInstagram' });
+      } else {
+        browser.tabs.sendMessage(tabId, { action: 'disableInstagram' });
+      }
+    });
+  });
 });
